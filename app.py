35c128eea4bb08a67bd42e5479aeae91fd372b0d
xxxx <xxxxxx@gmail.com>
2023-09-29 17:28:21 +0900

    [Add] index

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask , request , render_template
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -22,21 +23,57 @@ def browser_setup(browse_visually = "no"):
     return browser
 
 
-@app.route("/" , methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == 'POST' and request.form['start_scraping'] == "true":
-        browse_visually = request.form['browse_visually'] 
+    # if request.method == 'POST' and request.form['start_scraping'] == "true":
+    #     browse_visually = request.form['browse_visually'] 
 
-        searched_url = "https://www.bookoffonline.co.jp/"
-        driver = browser_setup(browse_visually)
-        driver.get(searched_url)
-        p_text = driver.find_element(By.TAG_NAME , "p").text
+    #     searched_url = "https://www.bookoffonline.co.jp/"
+    #     driver = browser_setup(browse_visually)
+    #     driver.get(searched_url)
+    #     p_text = driver.find_element(By.TAG_NAME , "p").text
 
-        return render_template("index.html" , p_text = p_text)
+    #     return render_template("index.html" , p_text = p_text)
 
-    else:
+    # else:
         return render_template("index.html")
+
+@app.route("/call_from_ajax", methods = ["POST"])
+def call_from_ajax():
+    if request.method == "POST":
+        # ここにPythonの処理を書く
+        try:
+            print(f"非同期処理開始")
+
+            searched_url = "https://www.bookoffonline.co.jp/"
+            driver = browser_setup()
+            driver.get(searched_url)
+            p_text = driver.find_element(By.TAG_NAME , "p").text
+
+            html_str = f"""
+            <h3>正常に取得できました</h3>
+            <div class="button019">
+                <h3>p_text : {p_text}</h3>
+            </div>
+            <p><br></p>
+        """
+        except Exception as e:
+            html_str = str(e)
+        dict = {"answer": html_str}      # 辞書
+    return json.dumps(dict)             # 辞書をJSONにして返す
+
+# @app.route("/call_from_ajax", methods = ["POST"])
+# def call_from_ajax():
+#     if request.method == "POST":
+#         # ここにPythonの処理を書く
+#         try:
+#             name = request.form["data"]
+#             message = f"フン。<b style='border: 2px solid red;'>{name}</b>というのかい。贅沢な名だねぇ。<br>"
+#         except Exception as e:
+#             message = str(e)
+#         dict = {"answer": message}      # 辞書
+#     return json.dumps(dict)             # 辞書をJSONにして返す
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=7777 , debug=True)
